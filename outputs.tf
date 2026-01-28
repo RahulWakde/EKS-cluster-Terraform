56446f060feca4ccea72abb179753871a7e10ba0
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-28 22:41:17 +0530

    Update outputs.tf
    change 2

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
 output "eks_cluster_name" {
-description = "EKS Cluster Name"
-value = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
-
 
 output "eks_cluster_endpoint" {
-description = "EKS Cluster Endpoint"
-value = module.eks.cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
-
